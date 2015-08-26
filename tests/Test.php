e227d801fdfc3568151510b91ecfaa128fbda77c
srph <seraphipod@gmail.com>
2015-08-27 03:10:13 +0800

    Add test case.

--- tests/Test.php
+++ tests/Test.php
@@ -21,4 +21,14 @@ class Test extends PHPUnit_Framework_TestCase {
 		SRPH\MapRange\map_range(function() use ($calls) { $calls++; }, $expected, $expected);
 		$this->assertEquals($calls, $expected);	
 	}
+
+	public function testFunctionShouldPassIndexAsFirstArgument() {
+		$stack = [];
+
+		SRPH\MapRange\map_range(function($i) use(&$stack) {
+			$stack[$i] = $i;
+		}, 1, 5);
+
+		$this->assertEquals($stack, ['1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5]);	
+	}
 }
